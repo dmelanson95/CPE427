2807012a4f5c361efa43d3d66475c40a35c18840
dmelanson95 <145362497+dmelanson95@users.noreply.github.com>
2023-11-03 09:13:29 -0400

    Add files via upload

---
+++ Lab 5/Lab5/Lab5/MyUART.c
@@ -0,0 +1,40 @@
+#include <avr/io.h>
+#include "MyUART.h"
+
+
+//UART Initialization
+void USART_init(UI thebaud){
+	UBRRH = (thebaud>>8);
+	UBRRL = (UC)(thebaud);
+	UCSRB = (1<<RXEN)|(1<<TXEN);
+	UCSRC = (1<<URSEL)|(1 << UCSZ0 )|(1 << UCSZ1 );
+}
+
+
+void USART_Transmit(UC data){
+	while (!(UCSRA & (1<<UDRE))); // Do nothing until UDR is ready for more data to be written to it
+	UDR = data;
+}
+
+
+UC USART_Receive(void){
+	while(!(UCSRA & (1<<RXC))); // Do nothing until data have been received and is ready to be read from UDR
+	return UDR; // Return the received byte value
+}
+
+
+void USART_Transmit_String(char *msg){
+	int k = 0;
+	while (msg[k] !=0) {
+		USART_Transmit(msg[k]);
+		k = k + 1;
+	}
+}
+
+
+void hex2ascii(UC inc, UC *c1, UC *c2){
+	(*c1)=asctab[(inc>>4)&0x0f];
+	(*c2)=asctab[inc& 0x0f];
+}
+
+
---
+++ Lab 5/Lab5/Lab5/main.c
@@ -0,0 +1,51 @@
+/*
+ * PWMTimer1Lab.c
+ * Updated to main.c in Lab5
+ *
+ * Created: 10/21/2021 8:48:09 AM
+ * Updated: 10/19/2023
+ * Original Code by : Example from slides: Brainiac Intervention3b-CPE2
+ * Utilized by David Melanson, Corbin Veitch
+ */ 
+
+
+#define F_CPU 8000000UL			// 8 MHz Crystal
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define duty_cycle25 63
+#define duty_cycle50 127
+#define duty_cycle75 191
+#define CPE_Relay (1<<PC4)
+#define CPE_Enable (1<<PD5)
+
+void send_CPE_Relay();
+void send_CPE_Enable();
+
+int main(void){
+	
+	DDRB |= (1<<DDB1);			//  1. Set PB1 to Output
+
+	TCCR1A  = (1 << WGM10);		//  2. Set Phase Correct PWM Mode 8bit
+	TCCR1A |= (1 << COM1A1);	//  3. Set None-Inverting Mode
+	TCCR1B  = (1 << CS10);		//  4. (No) Pre-Scaler
+
+	OCR1A = 255;
+	
+	while(1){};
+}
+
+void send_CPE_Relay(){
+	_delay_ms(1000);
+	DDRC |= CPE_Relay;
+	PORTC |= CPE_Relay;
+	_delay_ms(2000);
+	PORTC &= !(CPE_Relay);
+}
+
+void send_CPE_Enable(){
+	_delay_ms(1000);
+	DDRD |= CPE_Enable;
+	PORTD |= CPE_Enable;
+}
+
